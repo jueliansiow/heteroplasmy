e45b8cea9dac752cabb2ade3e2ee21219f29092c
jueliansiow <administrator@MEL-analysis.local>
2015-10-21 12:55:55 +1100

    Attempting to add options and unzipping
    Changed so that it finds .gz files specifically instead of fastq files.
    
    Added in a step to unzip the files but it takes a long time.
    
    Added in options and in the process to see if it works.

--- seqtksubtrimscript.sh
+++ seqtksubtrimscript.sh
@@ -1,34 +1,76 @@
 #!/bin/sh
 
 
-# How many reads do you want to end up with?
-read -e -p "Enter a fraction (in decimals) or the number of reads you would like: " numreads
+#options when running the script
+for i in "$@"
+do
+case $i in
+    -n=*|--num_reads=*)
+    NUM_READS="${i#*=}"
+    shift # past argument=value
+    ;;
+    -d=*|--directory=*)
+    DIRECTORY="${i#*=}"
+    shift # past argument=value
+    ;;
+    --default)
+    DEFAULT=YES
+    shift # past argument with no value
+    ;;
+    *)
+            # unknown option
+    ;;
+esac
+done
+echo "Number of reads  = ${NUM_READS}"
+echo "Directory        = ${DIRECTORY}"
+#echo "LIBRARY PATH    = ${LIBPATH}"
+#echo "Number files in SEARCH PATH with EXTENSION:" $(ls -1 "${SEARCHPATH}"/*."${EXTENSION}" | wc -l)
+#if [[ -n $1 ]]; then
+#    echo "Last line of file specified as non-opt/last argument:"
+#    tail -1 $1
+#fi
 
-
-#changing the file format from .fastq to .fq so that seqtk accepts the file. Apparently not needed.
-#for f in *.fastq; do mv $f `basename $f .fastq`.fq; done
 
 # find/define files and directories.
 basedir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-fwd_reads=$(find $basedir -name '*R1*')
-rev_reads=$(find $basedir -name '*R2*')
+fwd_reads=$(find $basedir -name '*R1*.gz')
+rev_reads=$(find $basedir -name '*R2*.gz')
 fwd_name=$(basename $fwd_reads)
 rev_name=$(basename $rev_reads)
 
+
 # Output file.
+fwd_unzip=$basedir"/unzip_$fwd_name"
+rev_unzip=$basedir"/unzip_$rev_name"
 fwd_trim=$basedir"/trim_$fwd_name"
 rev_trim=$basedir"/trim_$rev_name"
-fwd_subtrim=$basedir"/sub_$fwd_name"
-rev_subtrim=$basedir"/sub_$rev_name"
+fwd_subtrim=$basedir"/subtrim_$fwd_name"
+rev_subtrim=$basedir"/subtrim_$rev_name"
+
+
+# Unzip the files
+#unzip=$(gzcat $fwd_reads)
+
+
+# Changing the file format from .fastq to .fq so that seqtk accepts the file.
+# Apparently not needed. Can use to remove .gz from file name.
+# Need to be careful when choosing file names.
+# for f in *.fastq.gz; do mv $f `basename $f .fastq.gz`.fq; done
+
+
+# Interleave the pair end files
+#seqtk mergepe $fwd_reads $rev_reads
+
 
 # Run the quality trimming. Default is set to 0.05% probability.
-seqtk trimfq $fwd_reads > $fwd_trim
-seqtk trimfq $rev_reads > $rev_trim
+#seqtk trimfq $fwd_reads > $fwd_trim
+#seqtk trimfq $rev_reads > $rev_trim
 
 
 # Run the subset script.
-seqtk sample -s100 $fwd_trim $numreads > $fwd_subtrim
-seqtk sample -s100 $rev_trim $numreads > $rev_subtrim
+#seqtk sample -s100 $fwd_trim ${NUM_READS} > $fwd_subtrim
+#seqtk sample -s100 $rev_trim ${NUM_READS} > $rev_subtrim
 
 
 
