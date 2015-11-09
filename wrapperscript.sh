1d7bfc5b24fdb86a16d301e950a74d4db10f3dbd
jueliansiow <administrator@MEL-analysis.local>
2015-11-10 09:49:58 +1100

    Added in commands to run the ARC script

--- wrapperscript.sh
+++ wrapperscript.sh
@@ -10,23 +10,25 @@ function show_help()
     echo ""
     echo "./testingoptions.sh"
     echo "\t-h --help"
-    echo "\t-n --numreads= Enter the number of reads that you would like to end up with after sub sampling. The number can be a specific number of reads or a fraction of the reads in decimals."
+    echo "\t-n --num_reads= Enter the number of reads that you would like to end up with after sub sampling. The number can be a specific number of reads or a fraction of the reads in decimals."
     echo "\t-d --outdirectory= Enter the directory where files will be output to after analysis."
     echo "\t-f --folderdirectory= Enter the directory where the files are to be used for analysis."
     echo "\t-a --arc_config= Enter the path to the ARC config file that has been supplied with this script."
     echo "\t-r --arc_reference= Enter the path to the reference file that ARC needs for assembly"
     echo "\t-k --min_readlgth= Enter the mininum read length you would like to keep after trimming."
     echo "\t-t --trimscript= Enter the path to where the trimming script lives."
+    echo "\t-s --arcscript= Enter the path to where the arc script lives."
     echo ""
 }
 
-numreads=
+num_reads=
 outdirectory=
 arc_config=
 arc_reference=
 folderdirectory=
 trimscript=
 min_readlgth=
+arcscript=
 
 
 while :; do
@@ -35,21 +37,21 @@ while :; do
             show_help
             exit
             ;;
-        -n|--numreads)       # Takes an option argument, ensuring it has been specified.
+        -n|--num_reads)       # Takes an option argument, ensuring it has been specified.
             if [ -n "$2" ]; then
-                numreads=$2
+                num_reads=$2
                 shift 2
                 continue
             else
-                printf 'ERROR: "--numreads" requires a non-empty option argument.\n' >&2
+                printf 'ERROR: "--num_reads" requires a non-empty option argument.\n' >&2
                 exit 1
             fi
             ;;
-        --numreads=?*)
-            numreads=${1#*=} # Delete everything up to "=" and assign the remainder.
+        --num_reads=?*)
+            num_reads=${1#*=} # Delete everything up to "=" and assign the remainder.
             ;;
-        --numreads=)         # Handle the case of an empty --file=
-            printf 'ERROR: "--numreads" requires a non-empty option argument.\n' >&2
+        --num_reads=)         # Handle the case of an empty --file=
+            printf 'ERROR: "--num_reads" requires a non-empty option argument.\n' >&2
             exit 1
             ;;
         -d|--outdirectory)
@@ -143,6 +145,26 @@ while :; do
             shift
             break
             ;;
+        -s|--arcscript)
+            if [ -n "$2" ]; then
+                arcscript=$2
+                shift 2
+                continue
+            else
+                printf 'ERROR: "--arcscript" requires a non-empty option argument.\n' >&2
+                exit 1
+            fi
+            ;;
+        --arcscript=?*)
+            arcscript=${1#*=} # Delete everything up to "=" and assign the remainder.
+            ;;
+        --arcscript=)         # Handle the case of an empty --file=
+            printf 'ERROR: "--arcscript" requires a non-empty option argument.\n' >&2
+            exit 1
+            ;;        --)              # End of all options.
+            shift
+            break
+            ;;
 		-t|--trimscript)
             if [ -n "$2" ]; then
                 trimscript=$2
@@ -175,8 +197,8 @@ done
 
 
 ##### Suppose --numreads is a required option. Ensure the variable "file" has been set and exit if not.
-if [ -z "$numreads" ]; then
-    printf 'ERROR: option "--numreads FILE" not given. See --help.\n' >&2
+if [ -z "$num_reads" ]; then
+    printf 'ERROR: option "--num_reads FILE" not given. See --help.\n' >&2
     exit 1
 fi
 
@@ -210,14 +232,20 @@ if [ -z "$trimscript" ]; then
     exit 1
 fi
 
+if [ -z "$arcscript" ]; then
+    printf 'ERROR: option "--inputdirectory FILE" not given. See --help.\n' >&2
+    exit 1
+fi
+
 
 echo Out_directory=$outdirectory
 echo Folder_directory=$folderdirectory
-echo Number_of_reads=$numreads
+echo Number_of_reads=$num_reads
 echo Minium_read_length=$min_readlgth
 echo Path_to_ARC_config=$arc_config
 echo Path_to_ARC_reference=$arc_reference
 echo Path_to_trimming_script=$trimscript
+echo Path_to_arcscript=$arcscript
 
 
 
@@ -231,7 +259,10 @@ do
 	cd $f
 	sh $trimscript -i $(pwd) -m $min_readlgth -o $outdirectory/$f/trim
 	cd $folderdirectory
+	mkdir -p $outdirectory/$f/ARC
+	sh $arcscript -i $outdirectory/$f/trim -n $num_reads -o $outdirectory/$f/ARC -a $arc_config -r $arc_reference
 	echo "Completed processing $f file."
 	
 done
 
+echo "Completed processing all files in $folderdirectory"
